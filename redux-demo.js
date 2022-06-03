55bd7b224d29af3e1ea9a40d999312821d93e644
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-03 17:04:29 -0600

    Added 'increment' condition to counterReducer.

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,12 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  return state;
 }
 
 const store = redux.createStore(counterReducer);
